8dcaa10c07c5c09d067dedd5ae0b6afbfcc0d743
akanbi87 <akanbi87@gmail.com>
2011-11-05 16:43:47 +0000

    Carregar arquivo feito

--- DinaHelp/src/dinahelp/negocio/AjudaTextualNegocio.java
+++ DinaHelp/src/dinahelp/negocio/AjudaTextualNegocio.java
@@ -1,9 +1,11 @@
 package dinahelp.negocio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
-
 
 /**
  * @author Guilherme Taffarel Bergamin
@@ -12,16 +14,31 @@ import java.io.PrintWriter;
  */
 public class AjudaTextualNegocio {
 
-	private File arquivoTexto;
+    private File arquivoTexto;
 
-	public void geraArquivoAjudaTextual(String caminho, String nomeArquivo, String texto) {
-		try {
-			arquivoTexto = new File(caminho + "\\" + nomeArquivo);
-			FileWriter fw = new FileWriter(arquivoTexto);
-			PrintWriter pw = new PrintWriter(fw);
-			pw.write(texto);
-			fw.close();
-		} catch (Exception e) {
-		}
-	}
+    public void geraArquivoAjudaTextual(String caminho, String nomeArquivo, String texto) {
+        try {
+            arquivoTexto = new File(caminho + "\\" + nomeArquivo);
+            FileWriter fw = new FileWriter(arquivoTexto);
+            PrintWriter pw = new PrintWriter(fw);
+            pw.write(texto);
+            fw.close();
+        } catch (IOException e) {
+        }
+    }
+
+    public String carregarArquivoAjudaTextual(String caminho, String nomeArquivo) {
+        try {
+            arquivoTexto = new File(caminho + "\\" + nomeArquivo);
+            BufferedReader buffer = new BufferedReader(new FileReader(arquivoTexto));
+            String str = "";
+
+            while (buffer.ready()) {
+                str = str + buffer.readLine();
+            }
+            return str;
+        } catch (IOException e) {
+        }
+        return "";
+    }
 }
